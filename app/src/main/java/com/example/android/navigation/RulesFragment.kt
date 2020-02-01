1db82d2ee666750f1de30cb4dfe30dacfc685e1c
rbonnell <ryanbonnell@protonmail.com>
2020-02-01 12:14:38 -0700

    Added 'Rules' and 'About' buttons, navigation and data binding functionality

--- app/src/main/java/com/example/android/navigation/RulesFragment.kt
+++ app/src/main/java/com/example/android/navigation/RulesFragment.kt
@@ -21,13 +21,21 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.appcompat.app.AppCompatActivity
+import androidx.databinding.DataBindingUtil
 import androidx.fragment.app.Fragment
+import androidx.navigation.findNavController
+import com.example.android.navigation.databinding.FragmentRulesBinding
 
 class RulesFragment : Fragment() {
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
                               savedInstanceState: Bundle?): View? {
+        val binding = DataBindingUtil.inflate<FragmentRulesBinding>(inflater,
+                R.layout.fragment_rules, container, false)
         (activity as AppCompatActivity).supportActionBar?.title = getString(R.string.title_trivia_rules)
+        binding.playButtonRules.setOnClickListener { view: View ->
+            view.findNavController().navigate(R.id.action_rulesFragment_to_gameFragment)
+        }
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_rules, container, false)
+        return binding.root
     }
 }
